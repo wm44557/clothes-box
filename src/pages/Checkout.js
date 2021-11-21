b950116ae43d134c7e5953a8913a59052089fc3f
Marcin Wrzos <marwrz36@gmail.com>
2021-11-21 22:12:21 +0100

    update

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -47,7 +47,7 @@ const Checkout1 = () => {
 
 
                         <ul role="list" className="-my-6 divide-y divide-gray-200">
-                            {basket.map((product) => (
+                            {basket?.map((product) => (
                                 <li key={product.id} className="py-6 flex">
                                     <div
                                         className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
